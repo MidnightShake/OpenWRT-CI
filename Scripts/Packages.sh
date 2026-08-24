08b11d23d580f3450fed8a4a8f9fefafc73a6e95
MidnightShake <103491263+MidnightShake@users.noreply.github.com>
2026-08-24 12:12:57 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -81,6 +81,8 @@ UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac sing-box luci-app-homeproxy luci-app-timewol luci-app-wolplus luci-app-wolultra"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+UPDATE_PACKAGE "luci-app-lucky" "FloatingDream528/luci-app-lucky" "main" "" "lucky luci-app-lucky luci-i18n-lucky-zh-cn"
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
